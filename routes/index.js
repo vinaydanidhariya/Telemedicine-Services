efe6d9783785007ff5f2a4ff283e0d82d16d255e
rahul gondaliya <rahulgondaliya0182565@gmail.com>
2023-06-30 17:24:14 +0530

    "NEW DESING OF ADMIN PANEL IMPLIMATED"

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models');
+const db = require("../models");
 const { User } = db;
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
-    res.render('login/login',
-      {
-        title: 'LOGIN'
-      });
+    res.render("login/login", { layout: false });
   } catch (error) {
     console.log(error);
   }
 });
 
-router.post('/login', async function (req, res, next) {
+router.post("/login", async function (req, res, next) {
   try {
-    res.render('dashboard/dashboard',
-      {
-        title: 'Dashboard'
-      });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/dashboard", async function (req, res, next) {
+  try {
+    res.render("dashboard/dashboard", {
+      title: "Dashboard",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/add-doctors", async function (req, res, next) {
+  try {
+    res.render("doctors/add-doctors", {
+      title: "ADD-DOCTORS",
+    });
   } catch (error) {
     console.log(error);
   }
