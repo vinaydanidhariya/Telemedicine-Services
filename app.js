2049cfe3ca44390722fca89925f7837a9a19ffce
Nirav <>
2023-07-25 17:04:53 +0530

    Role module and indentation

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const path = require("path");
 const cors = require('cors');
 require('dotenv').config();
 const cookieParser = require("cookie-parser");
+global.checkAccess = require("./middleware/authorization").checkAccess;
 const indexRouter = require("./routes/index");
-const errorRouter = require("./routes/error.js");
 const adminRouter = require("./routes/admin/user");
 const usersRouter = require("./routes/users");
+const doctorAccess = require("./routes/doctor");
 const whatsappRouter = require("./routes/whatsapp-webhook/whatsapp-webhook");
 const razorPayROuter = require("./routes/payment-webhook/payment-webhook");
 const doctorRouter = require("./routes/admin/doctor.js")
@@ -55,20 +56,19 @@ app.use(cookieParser());
 // Add headers before the routes are defined
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
 });
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/whatsapp-payment", razorPayROuter);
 auth.login(app);
 // app.use('/onboarding', OnboardingRouter);
-app.use("/", indexRouter)
+app.use("/", indexRouter);
 app.use("/admin", adminRouter);
-app.use("/error", errorRouter);
 app.use("/admin/dashboard", dashboardRouter);
 app.use("/users", usersRouter);
 app.use("/wa-webhook", whatsappRouter);
@@ -79,42 +79,43 @@ app.use("/setting", settingsRouter);
 app.use("/blogs", blogsRouter);
 app.use("/events", eventsRouter);
 app.use('/upload', uploadRouter);
+app.use('/doctor', doctorAccess);
 
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.status(err.status || 500);
-  res.redirect("/error")
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+	res.status(err.status || 500);
+	res.redirect("/error")
 });
 module.exports = app;
 
 function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) return val;
-  if (port >= 0) return port;
-  return false;
+	const port = parseInt(val, 10);
+	if (isNaN(port)) return val;
+	if (port >= 0) return port;
+	return false;
 }
 function onError(error) {
-  if (error.syscall !== "listen") throw error;
-  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
+	if (error.syscall !== "listen") throw error;
+	const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+	switch (error.code) {
+		case "EACCES":
+			console.error(bind + " requires elevated privileges");
+			process.exit(1);
+			break;
+		case "EADDRINUSE":
+			console.error(bind + " is already in use");
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
 }
 function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
+	const addr = server.address();
+	const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+	debug("Listening on " + bind);
 }
--- routes/admin/Blog.js
+++ routes/admin/Blog.js
@@ -6,153 +6,153 @@ const authentication = require("../../middleware/login_module").check_auth;
 var multer = require("multer");
 
 var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + file.originalname.replace(/\s+/g, "-"));
-  },
+	destination: (req, file, cb) => {
+		cb(null, "public/images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + file.originalname.replace(/\s+/g, "-"));
+	},
 });
 
 const uploadSourceLeadFile = multer({
-  storage: storage, // global posting data upload file storage assign
-  limits: {
-    // Define limits for the file
-    fileSize: 5 * 1024 * 1024, // 5 mb image only allowed
-  },
-  fileFilter: function (req, file, cb) {
-    // Filter the file based on the type.
-    if (
-      file.mimetype == "image/png" ||
-      file.mimetype == "image/jpg" ||
-      file.mimetype == "image/jpeg"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
-    }
-  },
+	storage: storage, // global posting data upload file storage assign
+	limits: {
+		// Define limits for the file
+		fileSize: 5 * 1024 * 1024, // 5 mb image only allowed
+	},
+	fileFilter: function (req, file, cb) {
+		// Filter the file based on the type.
+		if (
+			file.mimetype == "image/png" ||
+			file.mimetype == "image/jpg" ||
+			file.mimetype == "image/jpeg"
+		) {
+			cb(null, true);
+		} else {
+			cb(null, false);
+			return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+		}
+	},
 }).single("blog_photo");
 
-router.get("/add-blog", authentication, function (req, res, next) {
-  try {
-    res.render("blogs/add-blog", {
-      title: "BLOG",
-      // layout: 'default'
-      sessionUser: req.user,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+router.get("/add-blog", authentication, checkAccess('add-blog'), function (req, res, next) {
+	try {
+		res.render("blogs/add-blog", {
+			title: "BLOG",
+			// layout: 'default'
+			sessionUser: req.user,
+		});
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 
 router.post("/add-blog", async function (req, res, next) {
-  try {
-    uploadSourceLeadFile(req, res, async function (err) {
-      // Check err while upload
-      if (err) {
-        console.log('Error while uploading image');
-        console.log(err);
-        return res.send({
-          type: 'error',
-          message: err.message
-        });
-      } else {
-        const { author_name, sort_description, description, title } = req.body;
-        console.log(req.body);
-        try {
-          db.Blogs.create({
-            title,
-            photo: req.file.filename,
-            description,
-            sortDescription: sort_description,
-            authorName: author_name,
-            date: new Date(),
-            updatedDate: new Date(),
-          })
-            .then(() => {
-              let message = `Blog Created successfully`;
-              res.send({
-                status: 200,
-                message,
-                type: "success",
-              });
-            })
-            .catch((error) => {
-              console.log(error);
-              res.send({
-                status: 400,
-                message: `Something Went Wrong while creating Blog`,
-                type: "fails",
-              });
-            });
-        } catch (error) {
-          console.log(error);
-          res.send({
-            status: 500,
-            message: error.message,
-            type: "error",
-          });
-        }
+	try {
+		uploadSourceLeadFile(req, res, async function (err) {
+			// Check err while upload
+			if (err) {
+				console.log('Error while uploading image');
+				console.log(err);
+				return res.send({
+					type: 'error',
+					message: err.message
+				});
+			} else {
+				const { author_name, sort_description, description, title } = req.body;
+				console.log(req.body);
+				try {
+					db.Blogs.create({
+						title,
+						photo: req.file.filename,
+						description,
+						sortDescription: sort_description,
+						authorName: author_name,
+						date: new Date(),
+						updatedDate: new Date(),
+					})
+						.then(() => {
+							let message = `Blog Created successfully`;
+							res.send({
+								status: 200,
+								message,
+								type: "success",
+							});
+						})
+						.catch((error) => {
+							console.log(error);
+							res.send({
+								status: 400,
+								message: `Something Went Wrong while creating Blog`,
+								type: "fails",
+							});
+						});
+				} catch (error) {
+					console.log(error);
+					res.send({
+						status: 500,
+						message: error.message,
+						type: "error",
+					});
+				}
 
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+			}
+		});
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 router.get('/', async (req, res) => {
-  try {
-    // Retrieve all blog posts from the database using the Blog model
-    const blogPosts = await db.Blogs.findAll({
-      attributes: ['blog_id', 'title', 'date', 'author_name', 'photo', 'sort_description', 'description'], // You can directly specify the attribute without an alias
-      raw: true,// Get raw JSON data instead of Sequelize instances
-    });
+	try {
+		// Retrieve all blog posts from the database using the Blog model
+		const blogPosts = await db.Blogs.findAll({
+			attributes: ['blog_id', 'title', 'date', 'author_name', 'photo', 'sort_description', 'description'], // You can directly specify the attribute without an alias
+			raw: true,// Get raw JSON data instead of Sequelize instances
+		});
 
-    console.log('Blog posts retrieved:', blogPosts);
+		console.log('Blog posts retrieved:', blogPosts);
 
-    res.render("blogs/blogs", {
-      title: "ChildDR | Setting",
-      blogPosts: blogPosts,
-      layout: "blank",
-      sessionUser: req.user,
-    });
-  } catch (error) {
-    console.error('Error retrieving blog posts:', error);
-    // Handle errors appropriately
-    res.status(500).send('Error retrieving blog posts'); // Return an error response to the client
-  }
+		res.render("blogs/blogs", {
+			title: "ChildDR | Setting",
+			blogPosts: blogPosts,
+			layout: "blank",
+			sessionUser: req.user,
+		});
+	} catch (error) {
+		console.error('Error retrieving blog posts:', error);
+		// Handle errors appropriately
+		res.status(500).send('Error retrieving blog posts'); // Return an error response to the client
+	}
 });
 
 router.get('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const blog = await db.Blogs.findByPk(id);
-    const blogPost = blog.toJSON();
+	try {
+		const { id } = req.params;
+		const blog = await db.Blogs.findByPk(id);
+		const blogPost = blog.toJSON();
 
-    if (!blogPost) {
-      // Blog post not found, handle error or redirect to an error page
-      res.status(404).send('Blog not found');
-      return;
-    }
+		if (!blogPost) {
+			// Blog post not found, handle error or redirect to an error page
+			res.status(404).send('Blog not found');
+			return;
+		}
 
-    res.render('blogs/blog-detail', {
-      title: blogPost.title,
-      description: blogPost.description,
-      date: blogPost.date,
-      authorName: blogPost.authorName,
-      photo: blogPost.photo,
-      layout: 'blank',
-      sessionUser: req.user,
-    });
-  } catch (error) {
-    console.error('Error retrieving blog post:', error);
-    // Handle errors appropriately
-    res.status(500).send('Error retrieving blog post');
-  }
+		res.render('blogs/blog-detail', {
+			title: blogPost.title,
+			description: blogPost.description,
+			date: blogPost.date,
+			authorName: blogPost.authorName,
+			photo: blogPost.photo,
+			layout: 'blank',
+			sessionUser: req.user,
+		});
+	} catch (error) {
+		console.error('Error retrieving blog post:', error);
+		// Handle errors appropriately
+		res.status(500).send('Error retrieving blog post');
+	}
 });
 
 
