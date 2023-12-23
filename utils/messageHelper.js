6f4a1c241b1ad0432c898e79334cc3e30fa21652
Nirav Gondaliya <niravgondaliya2565@gmail.com>
2023-12-23 21:43:44 +0530

    Update messageObject function to messageObject2

--- utils/messageHelper.js
+++ utils/messageHelper.js
@@ -914,7 +914,7 @@ const prefillform = {
 
 const sendTOCBlock = async (recipient) => {
 	try {
-		let newMessageObject = messageObject(recipient);
+		let newMessageObject = messageObject2(recipient);
 		newMessageObject.interactive = tocBlock;
 
 		axios.post(`https://graph.facebook.com/${apiVersion}/${myNumberId}/messages`, newMessageObject, {
